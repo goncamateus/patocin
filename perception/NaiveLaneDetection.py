cbda73d9348ec28d216a7e14da5f917918537082
carlos <cfcv@cin.ufpe.br>
2020-10-18 13:16:46 -0300

    documented the naive lane detection code

--- perception/NaiveLaneDetection.py
+++ perception/NaiveLaneDetection.py
@@ -16,19 +16,38 @@ class NaiveLaneDetection(Perception):
         self.hough_max_line_gap = hough_max_line_gap
 
     def Perceive(self, data):
+        """
+            Main function that will be called to output the detected lanes
+            composed of a seven-step pipeline.
+        """
         assert data.shape == (480,640,3), "NaiveLaneDetector must receive a (480,640,3) image"
+
+        #1. Transform the image to grayscale
         gray = self.GrayScale(data)
+
+        #2. Apply a gaussian blur
         g_blur = self.GaussianBlur(gray, self.gaussian_kernel)
+
+        #3. Use the Canny edge detector to find edges
         canny = self.Canny(g_blur, self.canny_low_threshold, self.canny_high_threshold)
+
+        #4. Select a region of interet
+        #As we are only interested in the lines immediatlly in front of the vehicle
+        #we do not process all the image, only the region of interest
         roi = self.RegionOfInterest(canny, self.vertices)
+
+        #5. Use the Hough transform to find lines
         line_image, lines = self.HoughLines(roi, self.hough_rho, self.hough_theta, self.hough_threshold, self.hough_min_line_len, self.hough_max_line_gap)
         if lines is not None:
+
+            #6. Separate the lines into left and right using its slope
             left_lines, right_lines, separed_lines_image = self.SeparateLines(line_image, lines)
+
+            #7. Average the left and right lines to obtain one estimative of the right and left lane
             final, left_line, right_line = self.AverageLines(left_lines, right_lines, separed_lines_image)
             return separed_lines_image, final, left_line, right_line
         else:
             return line_image, line_image, (None, None), (None, None)
-        #final = self.AverageLines(left_lines, right_lines, separed_lines_image)
 
     def GrayScale(self, img):
         """Function that transforms the color input image in a grayscale one"""
@@ -71,8 +90,8 @@ class NaiveLaneDetection(Perception):
 
     def HoughLines(self, img, rho, theta, threshold, min_line_len, max_line_gap):
         """
-        img should be the output of a Canny transform.
-        Returns an image with hough lines drawn.
+            img should be the output of a Canny transform.
+            Returns an image with hough lines drawn.
         """
         lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
         line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
@@ -117,6 +136,10 @@ class NaiveLaneDetection(Perception):
         return left, right, separed_lines
 
     def AverageLines(self, left_lines, right_lines, image):
+        """
+            Average left and right lines to get an estimative
+            of the left and right lane.
+        """
         line_image = np.zeros_like(image)
         
         left_line_p1 = None
@@ -128,6 +151,7 @@ class NaiveLaneDetection(Perception):
             a = []
             b = []
 
+            # Insert the slope and b coefficient of all lines into a list
             for line in left_lines:
                 for x1,y1,x2,y2 in line:
                     a_aux = (y2-y1) / (x2-x1)
@@ -135,11 +159,12 @@ class NaiveLaneDetection(Perception):
 
                     a.append(a_aux)
                     b.append(b_aux)
-            #Find the line(y = Ax + B) that best fit the points    
-            #left_A, left_B, _, _, _ = stats.linregress(pts_x, pts_y)
+
+            # Average the coefficients
             left_A = np.array(a).mean()
             left_B = np.array(b).mean()
 
+            # Draw the averaged line
             left_line_p1 = (0, int(left_B))
             left_line_p2 = (int((150-left_B)/left_A), 150)
             cv2.line(line_image, left_line_p1, left_line_p2, [255,0,0], 10)
@@ -148,6 +173,7 @@ class NaiveLaneDetection(Perception):
             a = []
             b = []
 
+            # Insert the slope and b coefficient of all lines into a List
             for line in right_lines:
                 for x1,y1,x2,y2 in line:
                     a_aux = (y2-y1) / (x2-x1)
@@ -155,11 +181,12 @@ class NaiveLaneDetection(Perception):
 
                     a.append(a_aux)
                     b.append(b_aux)
-            #Find the line(y = Ax + B) that best fit the points    
-            #left_A, left_B, _, _, _ = stats.linregress(pts_x, pts_y)
+
+            # Average the coefficients
             right_A = np.array(a).mean()
             right_B = np.array(b).mean()
 
+            # Draw the averaged line
             right_line_p1 = (639, int(right_A*639 + right_B))
             right_line_p2 = (int((150-right_B)/right_A), 150)
             cv2.line(line_image, right_line_p1, right_line_p2, [255,255,0], 10)
