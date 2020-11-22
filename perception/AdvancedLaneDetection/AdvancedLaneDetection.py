a52d6619c3fe0adaa64738f7ba36a7346db232aa
carlos <cfcv@cin.ufpe.br>
2020-11-22 19:42:56 -0300

    Create the AdvancedLaneDetection class

---
+++ perception/AdvancedLaneDetection/AdvancedLaneDetection.py
@@ -0,0 +1,12 @@
+import os
+os.sys.path.append("../../perception")
+
+class AdvancedLaneDetection(Perception):
+    def __init__(self, yellow_threshold, white_threshold):
+        self.yellow_threshold = yellow_threshold
+        self.white_threshold = white_threshold
+
+    def Perceive(self, data):
+        assert data.shape == (480, 640, 3), "AdvancedLaneDetector must receive a (480, 640, 3) image"
+
+        return data        
