d96fff5a7e2dbfcd063f7e2d496469e0fd35f44e
carlos <cfcv@cin.ufpe.br>
2020-11-22 21:10:44 -0300

    fix: import perception class

--- perception/AdvancedLaneDetection/AdvancedLaneDetection.py
+++ perception/AdvancedLaneDetection/AdvancedLaneDetection.py
@@ -1,6 +1,8 @@
 import os
 os.sys.path.append("../../perception")
 
+from Perception import Perception
+
 class AdvancedLaneDetection(Perception):
     def __init__(self, yellow_threshold, white_threshold):
         self.yellow_threshold = yellow_threshold
